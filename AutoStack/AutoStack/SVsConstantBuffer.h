2a31233d6379b349da71c4fd629304d4c5f8d5ee
bboczula <boczula.bartosz@gmail.com>
2017-02-08 03:53:17 -0800

    Fixed PS - VS signature mismatch error.

---
+++ AutoStack/AutoStack/SVsConstantBuffer.h
@@ -0,0 +1,7 @@
+#pragma once
+
+__declspec(align(16)) struct SVsConstantBuffer
+{
+	float width;
+	float height;
+};
--- AutoStack/AutoStack/ps.hlsl
+++ AutoStack/AutoStack/ps.hlsl
@@ -1,4 +1,4 @@
-float4 main(float4 position : POSITION) : SV_TARGET
+float4 main(float4 position : SV_POSITION) : SV_TARGET
 {
 	return float4(0.3019f, 0.7019f, 0.3450f, 1.0f);
 }
