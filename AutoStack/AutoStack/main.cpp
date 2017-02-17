882655e36b188320ab3b7bc0710a0a223a238a75
bboczula <boczula.bartosz@gmail.com>
2017-02-17 02:16:44 -0800

    Enabled drawing given number of elements

--- AutoStack/AutoStack/main.cpp
+++ AutoStack/AutoStack/main.cpp
@@ -21,8 +21,8 @@ public:
 private:
 	void initialize() override
 	{
-		const unsigned int NUM_OF_DAYS{ 50 };
-		const unsigned int START_AT{ 5540 };
+		const unsigned int NUM_OF_DAYS{ 100 };
+		const unsigned int START_AT{ 5590 - NUM_OF_DAYS };
 
 		SStock* temp = wig.at(START_AT);
 		double scopeMax = temp->max;
@@ -49,6 +49,13 @@ private:
 		for (int i = 0; i < NUM_OF_DAYS; i++)
 		{
 			temp = wig.at(START_AT + i);
+
+			// Params
+			int margin = 2;
+			int candleWidth = (800 / NUM_OF_DAYS) - 2 * margin;
+			int screenHeight = 600;
+
+			// Draw candle
 			double openPos = (temp->open - scopeMin) / scope;
 			double closePos = (temp->close - scopeMin) / scope;
 
@@ -57,15 +64,15 @@ private:
 			const int LEVEL_2{ 118 };
 			double maxPos = (temp->max - scopeMin) / scope;
 			double minPos = (temp->min - scopeMin) / scope;
-			SPoint x{ 15 + (15 * i), maxPos * 600, LEVEL_1 };
-			engine->drawQuadC(x, 1, (x.y - (minPos * 600)), 255, 255, 255);
+			SPoint x{ 10 + (candleWidth / 2.0f) + ((2 * margin + candleWidth) * i), maxPos * screenHeight, LEVEL_1 };
+			engine->drawQuadC(x, 1, (x.y - (minPos * screenHeight)), 255, 255, 255);
 
-			int close = static_cast<int>(closePos * 600);
-			int open = static_cast<int>(openPos * 600);
+			int close = static_cast<int>(closePos * screenHeight);
+			int open = static_cast<int>(openPos * screenHeight);
 			if (openPos > closePos)
 			{
-				SPoint p{ 10 + (15 * i), open, LEVEL_2 };
-				engine->drawQuadC(p, 10, (p.y - close), 255, 192, 0);
+				SPoint p{ 10 + ((2 * margin + candleWidth) * i), open, LEVEL_2 };
+				engine->drawQuadC(p, candleWidth, (p.y - close), 255, 192, 0);
 			}
 			else
 			{
@@ -73,8 +80,8 @@ private:
 				open = close;
 				close = temp;
 
-				SPoint p{ 10 + (15 * i), open, LEVEL_2 };
-				engine->drawQuadC(p, 10, (p.y - close), 172, 211, 254);
+				SPoint p{ 10 + ((2 * margin + candleWidth) * i), open, LEVEL_2 };
+				engine->drawQuadC(p, candleWidth, (p.y - close), 172, 211, 254);
 			}
 		}
 		//engine->drawTextureQuad({ 200, 300, 0 }, 200, 200);
